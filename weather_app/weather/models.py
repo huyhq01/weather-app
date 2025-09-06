b7e1b06d14fd6037457359b823f03333bfbb4dd2
huy diem dam <huyhoquang01@gmail.com>
2025-09-06 16:43:42 +0700

    feat: add weather model and initial migration

--- weather_app/weather/models.py
+++ weather_app/weather/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+class WeatherData(models.Model):
+    city = models.CharField(max_length=100)  
+    temperature = models.FloatField()  
+    cloud = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(100)]
+    )
+    description = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
